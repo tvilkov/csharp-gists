a248a6b3060431fb911f15d52cbbd3553c8b1bda
vilkov.timur@gmail.com <vilkov.timur@gmail.com>
2016-03-26 15:48:18 +0300

    Simple task throttle implementation

---
+++ src/CSharp.Gists.Tests/Tasks/TaskThrottleTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using CSharp.Gists.Tasks;
+using NUnit.Framework;
+
+namespace CSharp.Gists.Tests.Tasks
+{
+    [TestFixture]
+    public class TaskThrottleTests
+    {
+        [Test]
+        public async void CanThrottle()
+        {
+            const int concurrentTaskCount = 20;
+            const int concurencyLimit = 5;
+            var throttle = new TaskThrottle(concurencyLimit);
+
+            long runningTaskCount = 0;
+            bool invariantCondition = true;
+
+            // Run watcher
+            var stopEvent = new ManualResetEvent(false);
+            var watcher = new Thread(() =>
+                {
+                    while (!stopEvent.WaitOne(TimeSpan.FromMilliseconds(1)))
+                    {
+                        if (Interlocked.Read(ref runningTaskCount) <= concurencyLimit) continue;
+                        invariantCondition = false;
+                        break;
+                    }
+                }) { Priority = ThreadPriority.AboveNormal, IsBackground = true };
+            watcher.Start();
+
+            // Concurrently run tasks with throttlings
+            var tasks = new List<Task>(concurrentTaskCount);
+            Parallel.For(0, concurrentTaskCount, new ParallelOptions { MaxDegreeOfParallelism = 3 }, i =>
+                {
+                    var taskNumber = i + 1;
+                    var task = throttle.Throttle(() =>
+                        {
+                            Debug.WriteLine("Task {0} started", taskNumber);
+                            Interlocked.Increment(ref runningTaskCount);
+                            Thread.Sleep(TimeSpan.FromSeconds(3));
+                            Interlocked.Decrement(ref runningTaskCount);
+                            Debug.WriteLine("Task {0} done", taskNumber);
+                        });
+                    lock (tasks)
+                    {
+                        tasks.Add(task);
+                    }
+                });
+
+            await Task.WhenAll(tasks);
+            stopEvent.Set();
+            watcher.Join();
+            throttle.Dispose();
+
+            Assert.IsTrue(invariantCondition, string.Format("There was more than {0} tasks running at the same time", concurencyLimit));
+        }
+    }
+}
---
+++ src/CSharp.Gists/Tasks/TaskThrottle.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharp.Gists.Tasks
+{
+    /// <summary>
+    /// Provides throttling capabilities for tasks.
+    /// </summary>
+    [DebuggerDisplay("Throttle: {ActiveTaskCount} active tasks of {m_ConcurencyLimit} allowed")]
+    class TaskThrottle : IDisposable
+    {
+        private readonly int m_ConcurencyLimit;
+        private SemaphoreSlim m_Semaphore;
+        private CancellationTokenSource m_CancellationTokenSource;
+
+        public TaskThrottle(int concurencyLimit)
+        {
+            if (concurencyLimit <= 0)
+                throw new ArgumentException(@"concurencyLimit is expected to be a positive integer value", "concurencyLimit");
+            m_ConcurencyLimit = concurencyLimit;
+        }
+
+        public int ActiveTaskCount
+        {
+            [DebuggerStepThrough]
+            get { return m_ConcurencyLimit - m_Semaphore.CurrentCount; }
+        }
+
+        private void ensureInitialized()
+        {
+            LazyInitializer.EnsureInitialized(ref m_Semaphore, () => new SemaphoreSlim(m_ConcurencyLimit));
+            LazyInitializer.EnsureInitialized(ref m_CancellationTokenSource, () => new CancellationTokenSource());
+        }
+
+        public Task<T> Throttle<T>(Func<T> action)
+        {
+            if (action == null) throw new ArgumentNullException("action");
+
+            return Throttle(() => Task.Factory.StartNew(action), CancellationToken.None);
+        }
+
+        public Task<T> Throttle<T>(Func<T> action, CancellationToken cancellationToken)
+        {
+            if (action == null) throw new ArgumentNullException("action");
+
+            return Throttle(() => Task.Factory.StartNew(action), cancellationToken);
+        }
+
+        public Task Throttle(Action action)
+        {
+            if (action == null) throw new ArgumentNullException("action");
+
+            return Throttle(() => Task.Factory.StartNew(action), CancellationToken.None);
+        }
+
+        public Task Throttle(Action action, CancellationToken cancellationToken)
+        {
+            if (action == null) throw new ArgumentNullException("action");
+
+            return Throttle(() => Task.Factory.StartNew(action), cancellationToken);
+        }
+
+        public Task Throttle(Func<Task> actionFactory)
+        {
+            if (actionFactory == null) throw new ArgumentNullException("actionFactory");
+
+            return Throttle(actionFactory, CancellationToken.None);
+        }
+
+        public async Task Throttle(Func<Task> actionFactory, CancellationToken cancellationToken)
+        {
+            if (actionFactory == null) throw new ArgumentNullException("actionFactory");
+
+            ensureInitialized();
+
+            using (var tokenSource = CancellationTokenSource.CreateLinkedTokenSource(m_CancellationTokenSource.Token, cancellationToken))
+            {
+                await m_Semaphore.WaitAsync(tokenSource.Token);
+                try
+                {
+                    var task = actionFactory();
+                    Debug.Assert(task != null, "Null task returned from the factory");
+
+                    await task;
+                }
+                finally
+                {
+                    m_Semaphore.Release();
+                }
+            }
+        }
+
+        public Task<T> Throttle<T>(Func<Task<T>> actionFactory)
+        {
+            if (actionFactory == null) throw new ArgumentNullException("actionFactory");
+
+            return Throttle(actionFactory, CancellationToken.None);
+        }
+
+        public async Task<T> Throttle<T>(Func<Task<T>> actionFactory, CancellationToken cancellationToken)
+        {
+            if (actionFactory == null) throw new ArgumentNullException("actionFactory");
+
+            ensureInitialized();
+
+            using (var tokenSource = CancellationTokenSource.CreateLinkedTokenSource(m_CancellationTokenSource.Token, cancellationToken))
+            {
+                await m_Semaphore.WaitAsync(tokenSource.Token);
+                try
+                {
+                    var task = actionFactory();
+                    Debug.Assert(task != null, "Null task returned from the factory");
+
+                    var result = await task;
+                    return result;
+                }
+                finally
+                {
+                    m_Semaphore.Release();
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            m_CancellationTokenSource.Cancel();
+            m_Semaphore.Dispose();
+        }
+    }
+}
